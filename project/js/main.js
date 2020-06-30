9eb098443185729dc46755b31e5740984f47784f
kondik <kond1k12@gmail.com>
2020-06-30 19:57:09 +0300

    HW4 done

--- project/js/main.js
+++ project/js/main.js
@@ -107,3 +107,28 @@ class Basket {
 
 
 test = new Products();
+
+
+let mystr = `<p>One: 'Hi Mary.' Two: 'Oh, hi.'</p>
+<p>One: 'How are you doing?'</p>
+<p>Two: 'I'm doing alright. How about you?'</p>
+<p>One: 'Not too bad. The weather is great isn't it?'</p>
+<p>Two: 'Yes. It's absolutely beautiful today.'</p>
+<p>One: 'I wish it was like this more frequently.'</p>
+<p>Two: 'Me too.'</p>
+<p>One: 'So where are you going now?'</p>
+<p>Two: 'I'm going to meet a friend of mine at the department store'</p>
+<p>One: 'Going to do a little shopping?'</p>
+<p>Two: 'Yeah, I have to buy some presents for my parents.'</p>
+<p>One: 'What's the occasion?'</p>
+<p>Two: 'It's their anniversary.'</p>
+<p>One: 'That's great. Well, you better get going. You don't want to be late.'</p>
+<p>Two: 'I'll see you next time.'</p>
+<p>One: 'Sure.' Bye.'</p>`;
+
+// mystr.replace(/\B'/ig, "+")
+myFixedStr = mystr.replace(/\B'+|'\B/ig, '"');
+
+
+// console.log(mystr.match(/\B'/ig));
+document.querySelector(".hw4").insertAdjacentHTML('beforeend', myFixedStr);

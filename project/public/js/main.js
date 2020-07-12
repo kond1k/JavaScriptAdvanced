2730b05543aa6cba1f54996821ab167a298bca03
kondik <kond1k12@gmail.com>
2020-07-12 18:30:16 +0300

    HW7 done

--- project/public/js/main.js
+++ project/public/js/main.js
@@ -1,12 +1,10 @@
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-
 const app = new Vue({
     el: '#app',
     data: {
         userSearch: '',
     },
     methods: {
-        getJson(url){
+        getJson(url) {
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
@@ -15,40 +13,39 @@ const app = new Vue({
         },
         postJson(url, data) {
             return fetch(url, {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(result => result.json())
-              .catch(error => {
-                  this.$refs.error.setError(error);
-              });
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                }).then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                });
         },
         putJson(url, data) {
             return fetch(url, {
-                method: 'PUT',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(result => result.json())
-              .catch(error => {
-                  this.$refs.error.setError(error);
-              });
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                }).then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                });
         },
         deleteJson(url, data) {
             return fetch(url, {
-                method: 'DELETE',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(result => result.json())
-              .catch(error => {
-                  this.$refs.error.setError(error);
-              });
+                    method: 'DELETE',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                }).then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                });
         },
     },
 });
-
